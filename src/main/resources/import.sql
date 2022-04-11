b1187cab251917ee2c47c3f6983d79772ba0a1d7
Steve Poole <spoole167@googlemail.com>
2022-04-11 13:01:36 -0400

    added depabot

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO team (id, name, description) VALUES (200, 'Azul','Lord of Rain?');
-INSERT INTO team (id, name, description) VALUES (201, 'IBM','Croviding computers to royalty since 1704');
+INSERT INTO team (id, name, description) VALUES (201, 'IBM','Providing computers to royalty since 1704');
 INSERT INTO team (id, name, description) VALUES (202, 'The Audience','java developers who actually do the work');
